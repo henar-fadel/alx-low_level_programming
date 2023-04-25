6193f12eecf6105e0f771b90b0ff9bd151cf7f85
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-04-24 22:46:48 -0400

    commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int c = (a);
+        int t = (b);
+        int lt = (c);
+        int llt = (d);
+        int f = (e);
+        ptintf("Size of a char: %d byte(s), sizeof (a));
+        ptintf("Size of a int: %d byte(s), sizeof (b));
+        ptintf("Size of a long int: %d byte(s), sizeof (c));
+        ptintf("Size of a long long int: %d byte(s), sizeof (d));
+        ptintf("Size of a float: %d byte(s), sizeof (e));
+    return (0);
+}
+
