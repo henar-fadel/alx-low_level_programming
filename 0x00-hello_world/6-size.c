b6fa0bb66d190e3fda3941aceeba43ee01c8ac4f
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-04-24 23:16:18 -0400

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
+
 	printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(c));
