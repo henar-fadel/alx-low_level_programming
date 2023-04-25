39ccfec968c2e0d1006dcb018c6e49e88ddf8976
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-04-24 22:52:33 -0400

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
         int lt = (c);
         int llt = (d);
         int f = (e);
-        ptintf("Size of a char: %d byte(s), sizeof (a));
-        ptintf("Size of a int: %d byte(s), sizeof (b));
-        ptintf("Size of a long int: %d byte(s), sizeof (c));
-        ptintf("Size of a long long int: %d byte(s), sizeof (d));
-        ptintf("Size of a float: %d byte(s), sizeof (e));
+        ptintf("Size of a char: %d byte(s), (unsigned long)sizeof (a));
+        ptintf("Size of a int: %d byte(s), (unsigned long)sizeof (b));
+        ptintf("Size of a long int: %d byte(s), (unsigned long)sizeof (c));
+        ptintf("Size of a long long int: %d byte(s), (unsigned long)sizeof (d));
+        ptintf("Size of a float: %d byte(s),(unsigned long)sizeof (e));
     return (0);
 }
 
