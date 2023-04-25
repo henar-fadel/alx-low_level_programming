7132cda9c3127c795e1c7b196d672cf56fa9e025
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-04-24 23:02:13 -0400

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-        printf("Size of a char: %d byte(s)", (unsigned long)sizeof (a));
-        printf("Size of a int: %d byte(s)", (unsigned long)sizeof (b));
-        printf("Size of a long int: %d byte(s)", (unsigned long)sizeof (c));
-        printf("Size of a long long int: %d byte(s)", (unsigned long)sizeof (d));
-        printf("Size of a float: %d byte(s)",(unsigned long)sizeof (e));
+        printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof (a));
+        printf("Size of a int: %d byte(s)\n", (unsigned long)sizeof (b));
+        printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof (c));
+        printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof (d));
+        printf("Size of a float: %d byte(s)\n",(unsigned long)sizeof (e));
     return (0);
 }
 
