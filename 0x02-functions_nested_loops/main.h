f1b21a7036f389aca21f08a6012d4186040a615a
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-04-26 21:26:20 -0400

    commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x18(void);
+int _islower(int );
+int _isalpha(int );
+int print_sighn(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int,int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+
