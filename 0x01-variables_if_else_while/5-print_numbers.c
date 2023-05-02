e0d5bb6310580819cace80053a453509b35ff06e
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-05-01 22:21:50 -0400

    digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * discription: alphabet
+ * Return: 0 success
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	return (0);
+	printf("\n");
+}
