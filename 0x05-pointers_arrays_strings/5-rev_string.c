d5c260adcecd69987713c89497a24fa0f1bba8e7
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-05-03 22:48:48 -0400

    point

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - print reversed string
+ * @s: pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int 1, i;
+	char temp;
+
+	for (1 = 0; s[1] != '\0'; ++1)
+
+	for (i = 0; i < 1 / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[1 - 1 - i];
+		s[1 - 1 - i] = temp;
+	}
+}
