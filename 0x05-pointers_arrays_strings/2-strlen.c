e1b2568169ea29da4b48c57abdd551d46a6a9312
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-05-03 22:17:16 -0400

    point

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenghth
+ * @s: string parameter
+ * Return: lenghth string
+ */
+int _strlen(char *s)
+{
+	int counter;
+	for counter = 0; *s != '\0'; s++;
+	++counter;
+	return (counter);
+}
