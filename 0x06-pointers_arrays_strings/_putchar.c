692b206e96593d372ddfcb270085847e92733412
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-05-04 20:20:24 -0400

    commit.

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,5 +1,10 @@
 #include <unistd.h>
-
+/**
+ * _putchar - write char
+ * @c: the char
+ * Return: on success 1
+ * On error, -1 is returned
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
