b689812ebe3ff9d81028e71af6c87b4338808123
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-05-04 21:50:19 -0400

    point

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *string_toupper - change lowercase
+ * @str: string
+ * Return: string
+ */
+char *string_toupper(char *str)
+
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
