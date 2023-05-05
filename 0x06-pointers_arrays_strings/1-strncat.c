2b973118a7a85fa92abd96e01fb7ee11b42cd00b
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-05-04 20:48:52 -0400

    point

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncat - function
+ * @src: pointer
+ * @dest: pointer
+ * @n: most num of bytes
+ * Return: 1 true, 0 false
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+	while (dest[c])
+		c++;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	dest[c + i] = '\0';
+
+	return (dest);
+}
