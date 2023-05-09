55593717063624121cbf996c411c57918fb64d18
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-05-08 22:49:45 -0400

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strchr - fills memory
+ * @s: pointer
+ * @c: constant
+ * Return: pointer to s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int asd;
+
+	for (asd = 0; s[asd] >= '\0' ; asd++)
+	{
+		if (s[asd] == c)
+		{
+			return (s + asd);
+		}
+	}
+	return ('\0')
+}
