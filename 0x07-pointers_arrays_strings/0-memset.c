8913a53f024944ab35aa4ea564f2b7d9c16ab19e
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-05-08 21:34:21 -0400

    commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memset - fills memory
+ * @s: pointset
+ * @b: constant
+ * @n: maximam bytes
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int mat;
+	for (mat = 0; n > 0; mat++, n--)
+	{
+		s[mat] = b;
+	}
+	return (s);
+}
